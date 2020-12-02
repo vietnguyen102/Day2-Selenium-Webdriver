c136b53d79369f36255c3eead6a797c458030ba8
vietnm <vietnm7597@gmail.com>
2020-12-02 22:28:18 +0700

    Update testcase7.rb

--- testcase7.rb
+++ testcase7.rb
@@ -2,6 +2,7 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 driver.get "https://itmscoaching.herokuapp.com/form"
 driver.manage.window.maximize
+
 driver.find_element(:id, 'first-name').send_keys('iTMS')
 driver.find_element(:id, 'last-name').send_keys('Coaching')
 driver.find_element(:id, 'job-title').send_keys('QA')
@@ -12,7 +13,7 @@ options = driver.find_element(:id, 'select-menu')
 select_object = Selenium::WebDriver::Support::Select.new(options)
 select_object.select_by(:text, "2-4")
 
-
+driver.find_element(:id, 'datepicker').send_keys('27/10/2025')
 
 driver.find_element(:link_text, 'Submit').click
 sleep 2
